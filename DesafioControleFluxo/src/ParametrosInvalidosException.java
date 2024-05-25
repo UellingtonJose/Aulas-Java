62074576a38f12cb63e08d8dc3d163c00ebf97c9
UellingtonJose <uellington-jose@hotmail.com>
2024-05-25 19:34:35 -0300

    Adicionando arquivos do DesafioControleFluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+//package desafio;
+
+// Definição da classe de exceção
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
